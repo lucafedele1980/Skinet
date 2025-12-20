668e4aef75d9172f520d99cb5b632ddf801d569b
lucafedele1980 <lucafedele1980@gmail.com>
2025-12-20 13:39:26 +0100

    End of section 8

---
+++ client/src/app/shared/models/products.ts
@@ -0,0 +1,9 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    brand: string;
+    quantityInStock: number;
+}
