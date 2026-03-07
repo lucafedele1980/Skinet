73928bf8ce84e64272b0567e57916047d2ad6b31
lucafedele1980 <lucafedele1980@gmail.com>
2026-03-07 09:57:43 +0100

    End of section 16

---
+++ client/src/app/core/guards/empty-cart-guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AccountService } from '../services/account.service';
+import { map, of } from 'rxjs';
+import { CartService } from '../services/cart.service';
+import { SnackbarService } from '../services/snackbar.service';
+
+export const emptyCartGuard: CanActivateFn = (route, state) => {
+  const cartService = inject(CartService);
+  const router = inject(Router);
+  const snack = inject(SnackbarService);
+
+  if (!cartService.cart() || cartService.cart()?.items.length === 0) {
+    snack.error('Your cart is empty');
+    router.navigateByUrl('/cart');
+    return false;
+  }
+  return true; 
+};
