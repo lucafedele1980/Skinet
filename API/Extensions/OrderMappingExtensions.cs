2746e36d8e4865c59311f0e8a63d866a3f0f485b
lucafedele1980 <lucafedele1980@gmail.com>
2026-08-12 10:18:32 +0200

    End of section 17

---
+++ API/Extensions/OrderMappingExtensions.cs
@@ -0,0 +1,38 @@
+using API.DTOs;
+using Core.Entities.OrderAggregate;
+
+namespace API.Extensions;
+
+public static class OrderMappingExtensions
+{
+    public static OrderDto ToDto(this Order order)
+    {
+        return new OrderDto
+        {
+            Id = order.Id,
+            OrderDate = order.OrderDate,
+            BuyerEmail = order.BuyerEmail,
+            ShippingAddress = order.ShippingAddress,
+            DeliveryMethod = order.DeliveryMethod.Description,
+            PaymentSummary = order.PaymentSummary,
+            ShippingPrice = order.DeliveryMethod.Price,
+            OrderItems = order.OrderItems.Select(x => x.ToDto()).ToList(),
+            Subtotal = order.Subtotal,
+            Total = order.GetTotal(),
+            Status = order.Status.ToString(),
+            PaymentIntentId = order.PaymentIntentId
+        };
+    }
+
+    public static OrderItemDto ToDto(this OrderItem orderItem)
+    {
+        return new OrderItemDto
+        {
+            ProductId = orderItem.ItemOrdered.ProductId,
+            ProductName = orderItem.ItemOrdered.ProductName,
+            PictureUrl = orderItem.ItemOrdered.PictureUrl,
+            Price = orderItem.Price,
+            Quantity = orderItem.Quantity
+        };
+    }
+}
---
+++ Core/Specifications/OrderSpecification.cs
@@ -0,0 +1,20 @@
+using Core.Entities.OrderAggregate;
+
+namespace Core.Specifications;
+
+public class OrderSpecification : BaseSpecification<Order>
+{
+    public OrderSpecification(string email) : base(o => o.BuyerEmail == email)
+    {
+        AddInclude(x => x.OrderItems);
+        AddInclude(x => x.DeliveryMethod);
+        AddOrderByDescending(x => x.OrderDate);
+    }
+ 
+    public OrderSpecification(string email, int id) : base(o => o.BuyerEmail == email && o.Id == id)
+    {
+        AddInclude("OrderItems");
+        AddInclude("DeliveryMethod");
+    }
+
+}
