e77101cd83c7e9a09540e61b6bd2088fe70d8e44
lucafedele1980 <lucafedele1980@gmail.com>
2026-08-13 14:48:35 +0200

    End of section 18

--- client/src/app/core/services/account.service.ts
+++ client/src/app/core/services/account.service.ts
@@ -2,8 +2,8 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Address, User } from '../../shared/models/user';
-import { withIncrementalHydration } from '@angular/platform-browser';
 import { map, tap } from 'rxjs';
+import { SignalrService } from './signalr.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,12 +11,15 @@ import { map, tap } from 'rxjs';
 export class AccountService {
   baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
+  private signarService = inject(SignalrService);
   currentUser = signal<User | null>(null);
 
   login(values: any) {
     let params = new HttpParams();
     params = params.append('useCookies', true);
-    return this.http.post<User>(this.baseUrl + 'login', values, {params});
+    return this.http.post<User>(this.baseUrl + 'login', values, {params}).pipe(
+      tap(() => this.signarService.createHubConnection())
+    );
   }
 
   register(values: any) {
@@ -33,7 +36,9 @@ export class AccountService {
   }
 
   logout() {
-    return this.http.post(this.baseUrl + 'account/logout', {});
+    return this.http.post(this.baseUrl + 'account/logout', {}).pipe(
+      tap(() => this.signarService.stopHubConnection())
+    );
   }
 
   updateAddress(address: Address) {
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51T8g894iUCw0XsIigBdsQKx7psgIDpNSKBWA7pOpv6t8QtTK0f3KvhKRfdi85VIuFIZDDFu9AboB7zxYZP2aNF5i00zRbDSzwv'
 };
